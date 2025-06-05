24f89ad8ac86e8f02e6d9245cb112df09b55f9a5
sayed489 <cedit8528@gmail.com>
2025-06-05 09:23:50 +0530

    Delete api_save-credentials.js

--- api_save-credentials.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { username, password } = req.body;
-    console.log(`Username: ${username}, Password: ${password}`);
-    res.status(200).json({ message: 'Credentials saved' });
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-}
